4718bf82585639e04f1575aca88aa1b3f8b5a14a
brah4729 <brah4729@users.noreply.github.com>
2026-05-14 19:17:57 +0700

    adding power mode

--- modules/battery.nix
+++ modules/battery.nix
@@ -6,6 +6,15 @@ services.tlp = {
   settings = {
     START_CHARGE_THRESH_BAT0 = 75;
     STOP_CHARGE_THRESH_BAT0  = 80;
+
   };
+  PLATFORM_PROFILE_ON_BAT = "quiet";
+  PLATFORM_PROFILE_ON_AC  = "performance";   # ← changed this
+
+  CPU_ENERGY_PERF_POLICY_ON_BAT  = "power";
+  CPU_ENERGY_PERF_POLICY_ON_AC   = "performance";  # ← max perf on AC
+
+  CPU_BOOST_ON_BAT = 0;   # no turbo on battery
+  CPU_BOOST_ON_AC  = 1;   # full turbo on AC
 };
 }
