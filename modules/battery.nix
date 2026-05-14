e33b2acd055c41831e538c410d5ed692b89160d0
brah4729 <brah4729@users.noreply.github.com>
2026-05-14 19:20:26 +0700

    formatiing lol

--- modules/battery.nix
+++ modules/battery.nix
@@ -1,20 +1,24 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 {
-services.power-profiles-daemon.enable = false; # disable if using GNOME
-services.tlp = {
-  enable = true;
-  settings = {
-    START_CHARGE_THRESH_BAT0 = 75;
-    STOP_CHARGE_THRESH_BAT0  = 80;
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      # Battery threshold
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0  = 80;
 
+      # Platform profiles
+      PLATFORM_PROFILE_ON_BAT = "quiet";
+      PLATFORM_PROFILE_ON_AC  = "performance";
+
+      # CPU policy
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC  = "performance";
+
+      # Turbo boost
+      CPU_BOOST_ON_BAT = 0;
+      CPU_BOOST_ON_AC  = 1;
+    };
   };
-  PLATFORM_PROFILE_ON_BAT = "quiet";
-  PLATFORM_PROFILE_ON_AC  = "performance";   # ← changed this
-
-  CPU_ENERGY_PERF_POLICY_ON_BAT  = "power";
-  CPU_ENERGY_PERF_POLICY_ON_AC   = "performance";  # ← max perf on AC
-
-  CPU_BOOST_ON_BAT = 0;   # no turbo on battery
-  CPU_BOOST_ON_AC  = 1;   # full turbo on AC
-};
 }
