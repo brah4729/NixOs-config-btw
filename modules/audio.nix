066cad19efcb8bda409cd912c35793eef9a5153d
brah4729 <brah4729@users.noreply.github.com>
2026-05-15 15:10:15 +0700

    added something

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,13 +1,23 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 {
- security.rtkit.enable = true;
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+
+    # Tambah ini untuk fix sample rate
+    extraConfig.pipewire."92-low-latency" = {
+      context.properties = {
+        default.clock.rate = 48000;
+        default.clock.allowed-rates = [ 44100 48000 ];
+        default.clock.quantum = 1024;
+        default.clock.min-quantum = 32;
+        default.clock.max-quantum = 8192;
+      };
+    };
   };
   hardware.enableAllFirmware = true;
 }
-
