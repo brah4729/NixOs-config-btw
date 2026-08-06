4b00bec3ec95df11388955aac380202431548b15
brah4729 <brah4729@users.noreply.github.com>
2026-08-06 09:01:30 +0700

    fixed noctalia not running

--- home/modules/noctalia.nix
+++ home/modules/noctalia.nix
@@ -4,7 +4,7 @@
 
   programs.noctalia = {
     enable = true;
-    systemd.enable = false;   # runs it as a user service, auto-restarts on config change
+    systemd.enable = true;   # runs it as a user service, auto-restarts on config change
     settings = {
       # you can leave this empty for now and configure via the GUI,
       # or start filling in options once you've seen the defaults
